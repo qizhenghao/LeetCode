8b6e0c698a2ad8dc3f825dba8a5cab03f9d692ed
qizhenghao <qizhenghao258@163.com>
2017-02-24 11:21:11 +0800

    每日一题：写一个函数,求两个整数之和,要求在函数体内不得使用+、-、*、/四则 运算符号

---
+++ src/com/bruce/open/offer/Question_45.java
@@ -0,0 +1,27 @@
+package com.bruce.open.offer;
+
+/**
+ * Created by qizhenghao on 17/2/24.
+ *
+ * 写一个函数,求两个整数之和,要求在函数体内不得使用+、-、*、/四则 运算符号
+ */
+public class Question_45 {
+
+    public static void main(String[] args) {
+
+        System.out.println(add(10, 22));
+        System.out.println(add(8, 22));
+        System.out.println(add(3, 12));
+    }
+
+    public static int add(int num1, int num2) {
+
+        int tempNum1;
+        while (num2!=0) {
+            tempNum1 = num1 ^ num2;
+            num2 = (num1&num2) << 1;
+            num1 = tempNum1;
+        }
+        return num1;
+    }
+}
